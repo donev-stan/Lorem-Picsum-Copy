12af7a3305b92910a67816e2672fae2646658c70
donev-stan <77002111+donev-stan@users.noreply.github.com>
2021-05-09 15:55:53 +0300

    fresh start

--- Lorem Picsum SPA Vanilla JS/scripts/htmlElements.js
+++
@@ -1,9 +0,0 @@
-const htmlElements = {
-    root: () => document.querySelector('#root'),
-    navigation: () => document.querySelector('nav'),
-    inputURL: () => document.querySelector('input.inputURL'),
-    tryBtn: () => document.querySelector('.tryURL'),
-    previewImg: () => document.querySelector('.preview-img')
-};
-
-export default htmlElements;
